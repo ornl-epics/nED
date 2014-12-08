9b78959e8da3cd1034a87d50d936e2396f1479e6
Matthew Pearson <pearsonmr@ornl.gov>
2014-12-08 15:53:44 -0500

    BasePvaPlugin: add PVRecord locking around access to the PVRecord. According to the documention this is required.

--- nedApp/src/BasePvaPlugin.cpp
+++ nedApp/src/BasePvaPlugin.cpp
@@ -120,23 +120,37 @@ void BasePvaPlugin::postData(bool postNeutrons, bool postMetadata)
         // EPICSv4 uses POSIX EPOCH, v3 uses EPICS EPOCH
         epics::pvData::TimeStamp time(epics::pvData::posixEpochAtEpicsEpoch + m_pulseTime.secPastEpoch, m_pulseTime.nsec, m_neutronsPostSeq++);
 
-        m_pvNeutrons->beginGroupPut();
-        m_pvNeutrons->timeStamp.set(time);
-        m_pvNeutrons->proton_charge->put(m_pulseCharge);
-        m_postNeutronsCb(this, m_pvNeutrons);
-        m_pvNeutrons->endGroupPut();
+	m_pvNeutrons->lock();
+	try {
+	  m_pvNeutrons->beginGroupPut();
+	  m_pvNeutrons->timeStamp.set(time);
+	  m_pvNeutrons->proton_charge->put(m_pulseCharge);
+	  m_postNeutronsCb(this, m_pvNeutrons);
+	  m_pvNeutrons->endGroupPut();
+	} catch (std::exception &e) {
+	  LOG_ERROR("Exception caught in BasePvaPlugin::postData, postNeutrons: %s.", e.what());
+	  m_pvNeutrons->unlock();
+	}
+	m_pvNeutrons->unlock();
     }
 
     if (postMetadata) {
         // EPICSv4 uses POSIX EPOCH, v3 uses EPICS EPOCH
         epics::pvData::TimeStamp time(epics::pvData::posixEpochAtEpicsEpoch + m_pulseTime.secPastEpoch, m_pulseTime.nsec, m_metadataPostSeq++);
 
-        m_pvMetadata->beginGroupPut();
-        m_pvMetadata->timeStamp.set(time);
-        m_pvMetadata->proton_charge->put(m_pulseCharge);
-        m_pvMetadata->time_of_flight->replace(freeze(m_cacheMeta.time_of_flight));
-        m_pvMetadata->pixel->replace(freeze(m_cacheMeta.pixel));
-        m_pvMetadata->endGroupPut();
+	m_pvMetadata->lock();
+	try {
+	  m_pvMetadata->beginGroupPut();
+	  m_pvMetadata->timeStamp.set(time);
+	  m_pvMetadata->proton_charge->put(m_pulseCharge);
+	  m_pvMetadata->time_of_flight->replace(freeze(m_cacheMeta.time_of_flight));
+	  m_pvMetadata->pixel->replace(freeze(m_cacheMeta.pixel));
+	  m_pvMetadata->endGroupPut();
+	} catch (std::exception &e) {
+	  LOG_ERROR("Exception caught in BasePvaPlugin::postData, portMetadata: %s.", e.what());
+	  m_pvNeutrons->unlock();
+	}
+	m_pvMetadata->unlock();
 
         // Reduce gradual memory reallocation by pre-allocating instead of clear()
         m_cacheMeta.time_of_flight.reserve(CACHE_SIZE);
