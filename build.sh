a866c14a6466e2e265d11b15e7374151273e0a53
Bogdan Vacaliuc <vacaliucb@ornl.gov>
2015-01-26 02:09:33 -0500

    keep build.sh up-to-date

--- build.sh
+++ build.sh
@@ -2,19 +2,11 @@
 cwd=`pwd`
 prefix=BL100:Det2
 controls=/home/controls/bl100
-stpath=bl100-Det2-nED/iocBoot/iocbl100-Det2-nED/st.cmd
+ioc=bl100-Det2-nED
+stpath=${ioc}/iocBoot/ioc${ioc}/st.cmd
 make clean
-pushd nedApp/src
-make epicsdb
 make
-popd
-pushd nedApp/Db
-make
-popd
-exit 0
 pushd pvtable
-./instantiate.py -b ${prefix} -o ${controls}/opi ${controls}/applications/${stpath} DspPlugin_v*.pvst
-./instantiate.py -b ${prefix} -o ${controls}/opi ${controls}/applications/${stpath} FemPlugin_v*.pvst
-./instantiate.py -b ${prefix} -o ${controls}/opi ${controls}/applications/${stpath} RocPlugin_v*.pvst
+./create_ioc_pvtables.py -b ${prefix} -o ${controls}/pvtable/${ioc} -n ${cwd} ${controls}/applications/${stpath}
 popd
 
