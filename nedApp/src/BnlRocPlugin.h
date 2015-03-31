f8726ec8523597df2d9f83517fa5863a4d5c6a7b
Greg Guyotte <guyottegs@ornl.gov>
2015-03-31 12:47:25 -0400

    Initial commit for BNLROC Support
    Based on BNLROC operations document, initial support for the BNLROC plugin
    has been coded.  This code is not yet tested against BNLROC hardware.
    
    TODO:  Add support to RocPvaPlugin for BNLROC Raw and Extended data formats.
    TODO:  Test with BNLROC hardware in lab.

---
+++ nedApp/src/BnlRocPlugin.h
@@ -0,0 +1,136 @@
+/* BnlRocPlugin.h
+ *
+ * Copyright (c) 2015 Oak Ridge National Laboratory.
+ * All rights reserved.
+ * See file LICENSE that is included with this distribution.
+ *
+ * @author Greg Guyotte
+ */
+
+#ifndef BNL_ROC_PLUGIN_H
+#define BNL_ROC_PLUGIN_H
+
+#include "BaseModulePlugin.h"
+#include "Fifo.h"
+
+#include <list>
+
+/**
+ * Plugin for BNL ROC module.
+ *
+ * The BnlRocPlugin extends BaseModulePlugin with the ROC board specifics.
+ * It handles DISCOVER response to verify module type and READ_VERSION
+ * which parses the version information.
+ * Several firmware version are supported:
+ *
+ * * BNL ROC hw=0.1 fw=0.3 as v03
+ *
+ * In general firmware versions differ in status or configuration
+ * parameters they provide. Those are available in individual files
+ * called BnlRocPlugin_<version>.cpp. BnlRocPlugin will refuse to communicate
+ * with unsupported versions.
+ *
+ */
+class BnlRocPlugin : public BaseModulePlugin {
+    public: // variables
+        static const int defaultInterfaceMask = BaseModulePlugin::defaultInterfaceMask | asynOctetMask;
+        static const int defaultInterruptMask = BaseModulePlugin::defaultInterruptMask | asynOctetMask;
+
+    private: // structures and definitions
+        static const unsigned NUM_BNLROCPLUGIN_DYNPARAMS;      //!< Maximum number of asyn parameters, including the status and configuration parameters
+
+    private: // variables
+        std::string m_version;                              //!< Version string as passed to constructor
+
+    public: // functions
+
+        /**
+         * Constructor for BnlRocPlugin
+         *
+         * Constructor will create and populate PVs with default values.
+         *
+         * @param[in] portName asyn port name.
+         * @param[in] dispatcherPortName Name of the dispatcher asyn port to connect to.
+         * @param[in] hardwareId Hardware ID of the BNLROC module, can be in IP format (xxx.xxx.xxx.xxx) or
+         *                       in hex number string in big-endian byte order (0x15FACB2D equals to IP 21.250.203.45)
+         * @param[in] version BNLROC HW&SW version, ie. V5_50
+         * @param[in] blocking Flag whether the processing should be done in the context of caller thread or in background thread.
+         */
+        BnlRocPlugin(const char *portName, const char *dispatcherPortName, const char *hardwareId, const char *version, int blocking=0);
+
+        /**
+         * Process RS232 packets only, let base implementation do the rest.
+         */
+        bool processResponse(const DasPacket *packet);
+
+        /**
+         * Send string/byte data to PVs
+         */
+        asynStatus readOctet(asynUser *pasynUser, char *value, size_t nChars, size_t *nActual, int *eomReason);
+
+        /**
+         * Receive string/byte data to PVs
+         */
+        asynStatus writeOctet(asynUser *pasynUser, const char *value, size_t nChars, size_t *nActual);
+
+        /**
+         * Try to parse the BNLROC version response packet an populate the structure.
+         *
+         * Function will parse all known BNLROC version responses and populate the
+         * version structure. If the function returns false, it does not recognize
+         * the response.
+         *
+         * @param[in] packet to be parsed
+         * @param[out] version structure to be populated
+         * @param[in] expectedLen expected size of the version response, used to
+         *                        verify the parsed packet matches this one
+         * @return true if succesful, false if version response packet could not be parsed.
+         */
+        static bool parseVersionRsp(const DasPacket *packet, BaseModulePlugin::Version &version);
+
+        /**
+         * Member counterpart of parseVersionRsp().
+         *
+         * @see BnlRocPlugin::parseVersionRsp()
+         */
+        bool parseVersionRspM(const DasPacket *packet, BaseModulePlugin::Version &version)
+        {
+            return parseVersionRsp(packet, version);
+        }
+
+        /**
+         * Configured version must match actual.
+         *
+         * @return true when they match, false otherwise.
+         */
+        bool checkVersion(const BaseModulePlugin::Version &version);
+
+    private: // functions
+
+        /**
+         * Handle READ_CONFIG response.
+         *
+         * For normal firmwares the function simply invokes BaseModulePlugin::rspReadConfig()
+         * passing it the original packet.
+         */
+        bool rspReadConfig(const DasPacket *packet);
+
+        /**
+         * Create and register all status BNLROC v0.0 parameters to be exposed to EPICS.
+         */
+        void createStatusParams_v00();
+
+        /**
+         * Create and register all config BNLROC v0.0 parameters to be exposed to EPICS.
+         */
+        void createConfigParams_v00();
+
+        /**
+         * Create and register all status counter BNLROC v0.0 parameters to be exposed to EPICS.
+         */
+        //void createCounterParams_v00();
+
+    protected:
+};
+
+#endif // BNL_ROC_PLUGIN_H
