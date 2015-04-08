f7f9e0bea4f2f3023d1b08bdd4b33801a23b177e
Klemen Vodopivec <kvx@ornl.gov>
2015-04-08 13:41:12 -0400

    Changed test pattern registers for ROC 5.6 and 5.7 only.
    As discussed today with Vlad and Bogdan we'll leave ROC 5.6 and ROC 5.7
    with bit 14 as alternate test pattern generator, in addition to bit 15
    as a global test pattern switch. This should be corrected in newer
    ROC versions, but since deployed on VISION, CORELLI and USANS we'll
    update the software for now.
    When enabled, ramp test pattern generator will override any other
    data produced by ROC including alternate test pattern generator which
    makes this solution work for the test pattern analyzer.

--- nedApp/src/RocPlugin_v56.cpp
+++ nedApp/src/RocPlugin_v56.cpp
@@ -628,7 +628,8 @@ void RocPlugin::createConfigParams_v56()
     createConfigParam("Reset",            'F', 0x0,  1, 0,  0);     // Reset enable                  (0=disable,1=enable)
 
     createConfigParam("TestPatternEn",    'F', 0x1,  1, 15, 0);     // Test pattern enable           (0=disable,1=enable)
-    createConfigParam("TestPatternDebug", 'F', 0x1,  3, 12, 0);     // Engineering Use only
+    createConfigParam("TestPatternAltEn", 'F', 0x1,  1, 14, 0);     // Alternate test pattern enable (0=disable,1=enable)
+    createConfigParam("TestPatternDebug", 'F', 0x1,  2, 12, 0);     // Engineering Use only
     createConfigParam("TestPatternId",    'F', 0x1, 12, 0,  0);     // Test pattern id
     createConfigParam("TestPatternRate",  'F', 0x2, 16, 0,  0);     // Test pattern rate             (65535=153 ev/s,9999=1 Kev/s,4999=2 Kev/s,1999=5 Kev/s,999=10 Kev/s,399=25 Kev/s,199=50 Kev/s,99=100 Kev/s,13=800 Kev/s,9=1 Mev/s,4=2 Mev/s,1=5 Mev/s,0=10 Mev/s)
 }
