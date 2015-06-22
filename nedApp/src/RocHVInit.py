ffd3cc609f65a8a230cffbb501f6eeb915f13c82
Carl Lionberger <lionbergerca@ornl.gov>
2015-06-22 15:09:48 -0400

    RocHVInit.py does some of the work of preparing the ROCHV PVs based on contents of the ROC HV supply registers.

---
+++ nedApp/src/RocHVInit.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+
+import sys
+import cothread
+from cothread.catools import *
+
+if len(sys.argv) < 2 :
+   print 'USAGE:', sys.argv[0], '<PV prefix>'
+   sys.exit()
+prefix = sys.argv[1]
+
+def transferValue(srcPV, destPV):
+   inp = caget(prefix + srcPV)
+   caput(prefix + destPV, inp)
+
+
+transferValue('DConvX', 'DConvSetX')
+transferValue('DConvY', 'DConvSetY')
+transferValue('DConvZ', 'DConvSetZ')
+
+transferValue('VConvX', 'VConvSetX')
+transferValue('VConvY', 'VConvSetY')
+transferValue('VConvZ', 'VConvSetZ')
+
+transferValue('RampRate', 'RampRateSet')
+
+caput(prefix + 'Volt.HIGH', 2300)
