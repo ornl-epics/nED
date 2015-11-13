3b7258e9c909998302a0085664b77e4160a1b42c
Klemen Vodopivec <kvx@ornl.gov>
2015-11-13 09:07:46 -0500

    DebugPlugin supports editing raw packet values.

--- nedApp/src/DebugPlugin.cpp
+++ nedApp/src/DebugPlugin.cpp
@@ -9,6 +9,7 @@
 
 #include "BaseModulePlugin.h"
 #include "DebugPlugin.h"
+#include "Common.h"
 #include "Log.h"
 
 #include <algorithm>
@@ -20,14 +21,13 @@ EPICS_REGISTER_PLUGIN(DebugPlugin, 2, "Port name", string, "Dispatcher port name
 DebugPlugin::DebugPlugin(const char *portName, const char *dispatcherPortName, int blocking)
     : BasePlugin(portName, dispatcherPortName, REASON_OCCDATA, blocking, NUM_GENERICMODULEPLUGIN_PARAMS, 1,
                  defaultInterfaceMask, defaultInterruptMask)
-    , m_hardwareId(0)
     , m_payloadLen(0)
     , m_expectedResponse(static_cast<DasPacket::CommandType>(0))
-    , m_lastConfigPayloadLen(0)
 {
-    createParam("ReqDest",      asynParamOctet, &ReqDest);      // WRITE - Module address to communicate with
-    createParam("ReqCmd",       asynParamInt32, &ReqCmd);       // WRITE - Command to be sent to module
-    createParam("ReqIsDsp",     asynParamInt32, &ReqIsDsp);     // WRITE - Is the module we communicate with behinds the DSP, using LVDS link
+    createParam("ReqDest",      asynParamOctet, &ReqDest, 0x0); // WRITE - Module address to communicate with
+    createParam("ReqCmd",       asynParamInt32, &ReqCmd, 0x0);  // WRITE - Command to be sent to module
+    createParam("ReqIsDsp",     asynParamInt32, &ReqIsDsp, 0);  // WRITE - Is the module we communicate with behinds the DSP, using LVDS link
+    createParam("ReqSend",      asynParamInt32, &ReqSend);      // WRITE - Send cached packet
     createParam("RspCmd",       asynParamInt32, &RspCmd);       // READ - Response command (see DasPacket::CommandType)
     createParam("RspCmdAck",    asynParamInt32, &RspCmdAck);    // READ - Response ACK/NACK
     createParam("RspHwType",    asynParamInt32, &RspHwType);    // READ - Hardware type (see DasPacket::ModuleType)
@@ -38,25 +38,96 @@ DebugPlugin::DebugPlugin(const char *portName, const char *dispatcherPortName, i
     createParam("RspDataLen",   asynParamInt32, &RspDataLen);   // READ - Response payload length in bytes
     createParam("RspData",      asynParamOctet, &RspData);      // READ - Response payload
     createParam("ByteGrp",      asynParamInt32, &ByteGrp);      // WRITE - Byte grouping mode
-    createParam("Channel",      asynParamInt32, &Channel, 0);   // WRITE - Select channel to send command to (read/write config only)
+    createParam("Channel",      asynParamInt32, &Channel, 0);   // Raw packet dword 0
+    createParam("RawPkt0",      asynParamInt32, &RawPkt0, 0);   // Raw packet dword 0
+    createParam("RawPkt1",      asynParamInt32, &RawPkt1, 0);   // Raw packet dword 1
+    createParam("RawPkt2",      asynParamInt32, &RawPkt2, 0);   // Raw packet dword 2
+    createParam("RawPkt3",      asynParamInt32, &RawPkt3, 0);   // Raw packet dword 3
+    createParam("RawPkt4",      asynParamInt32, &RawPkt4, 0);   // Raw packet dword 4
+    createParam("RawPkt5",      asynParamInt32, &RawPkt5, 0);   // Raw packet dword 5
+    createParam("RawPkt6",      asynParamInt32, &RawPkt6, 0);   // Raw packet dword 6
+    createParam("RawPkt7",      asynParamInt32, &RawPkt7, 0);   // Raw packet dword 7
+    createParam("RawPkt8",      asynParamInt32, &RawPkt8, 0);   // Raw packet dword 8
+    createParam("RawPkt9",      asynParamInt32, &RawPkt9, 0);   // Raw packet dword 9
+    createParam("RawPkt10",     asynParamInt32, &RawPkt10, 0);  // Raw packet dword 10
+    createParam("RawPkt11",     asynParamInt32, &RawPkt11, 0);  // Raw packet dword 11
+    createParam("RawPkt12",     asynParamInt32, &RawPkt12, 0);  // Raw packet dword 12
+    createParam("RawPkt13",     asynParamInt32, &RawPkt13, 0);  // Raw packet dword 13
+    createParam("RawPkt14",     asynParamInt32, &RawPkt14, 0);  // Raw packet dword 14
+    createParam("RawPkt15",     asynParamInt32, &RawPkt15, 0);  // Raw packet dword 15
+    createParam("RawPkt16",     asynParamInt32, &RawPkt16, 0);  // Raw packet dword 16
+    createParam("RawPkt17",     asynParamInt32, &RawPkt17, 0);  // Raw packet dword 17
 
     callParamCallbacks();
+
+    generatePacket();
 }
 
 asynStatus DebugPlugin::writeInt32(asynUser *pasynUser, epicsInt32 value)
 {
-    if (pasynUser->reason == ReqCmd) {
-        // Allow any value from the client.
-        request(static_cast<const DasPacket::CommandType>(value & 0xFF));
+    if (pasynUser->reason == ReqSend) {
+        if (value == 1)
+            sendPacket();
         return asynSuccess;
+    } else if (pasynUser->reason == ReqIsDsp) {
+        setIntegerParam(ReqIsDsp, value);
+        generatePacket();
+        return asynSuccess;
+    } else if (pasynUser->reason == ReqCmd) {
+        setIntegerParam(ReqCmd, value);
+        generatePacket();
+        return asynSuccess;
+    } else if (pasynUser->reason == RawPkt0) {
+        m_rawPacket[0] = value;
+    } else if (pasynUser->reason == RawPkt1) {
+        m_rawPacket[1] = value;
+    } else if (pasynUser->reason == RawPkt2) {
+        m_rawPacket[2] = value;
+    } else if (pasynUser->reason == RawPkt3) {
+        value = ALIGN_UP(value, 4);
+        if (value > 12*4)
+            value = 12*4;
+        m_rawPacket[3] = value;
+    } else if (pasynUser->reason == RawPkt4) {
+        m_rawPacket[4] = value;
+    } else if (pasynUser->reason == RawPkt5) {
+        m_rawPacket[5] = value;
+    } else if (pasynUser->reason == RawPkt6) {
+        m_rawPacket[6] = value;
+    } else if (pasynUser->reason == RawPkt7) {
+        m_rawPacket[7] = value;
+    } else if (pasynUser->reason == RawPkt8) {
+        m_rawPacket[8] = value;
+    } else if (pasynUser->reason == RawPkt9) {
+        m_rawPacket[9] = value;
+    } else if (pasynUser->reason == RawPkt10) {
+        m_rawPacket[10] = value;
+    } else if (pasynUser->reason == RawPkt11) {
+        m_rawPacket[11] = value;
+    } else if (pasynUser->reason == RawPkt12) {
+        m_rawPacket[12] = value;
+    } else if (pasynUser->reason == RawPkt13) {
+        m_rawPacket[13] = value;
+    } else if (pasynUser->reason == RawPkt14) {
+        m_rawPacket[14] = value;
+    } else if (pasynUser->reason == RawPkt15) {
+        m_rawPacket[15] = value;
+    } else if (pasynUser->reason == RawPkt16) {
+        m_rawPacket[16] = value;
+    } else if (pasynUser->reason == RawPkt17) {
+        m_rawPacket[17] = value;
     }
     return BasePlugin::writeInt32(pasynUser, value);
 }
 
 asynStatus DebugPlugin::writeOctet(asynUser *pasynUser, const char *value, size_t nChars, size_t *nActual)
 {
-    if (pasynUser->reason == ReqDest)
-        m_hardwareId = BaseModulePlugin::ip2addr(std::string(value, nChars));
+    if (pasynUser->reason == ReqDest) {
+        setStringParam(ReqDest, value);
+        generatePacket();
+        *nActual = nChars;
+        return asynSuccess;
+    }
     return BasePlugin::writeOctet(pasynUser, value, nChars, nActual);;
 }
 
@@ -96,50 +167,69 @@ asynStatus DebugPlugin::readOctet(asynUser *pasynUser, char *value, size_t nChar
     return BasePlugin::readOctet(pasynUser, value, nChars, nActual, eomReason);
 }
 
-void DebugPlugin::request(const DasPacket::CommandType command)
+void DebugPlugin::generatePacket()
 {
     DasPacket *packet;
-    int isDsp;
-    int channel = 0;
+    int cmd, isDsp;
+    char destIp[20];
+    getIntegerParam(ReqCmd,     &cmd);
+    getIntegerParam(ReqIsDsp,   &isDsp);
+    getStringParam(ReqDest,     sizeof(destIp), destIp);
 
-    if (m_hardwareId == 0)
-        return;
-
-    (void)getIntegerParam(ReqIsDsp, &isDsp);
-    (void)getIntegerParam(Channel, &channel);
-
-    // It should be 0 already if last response was not CMD_READ_CONFIG
-    if (command != DasPacket::CMD_WRITE_CONFIG)
-        m_lastConfigPayloadLen = 0;
-
+    int dest = BaseModulePlugin::ip2addr(std::string(destIp, sizeof(destIp)));
+    DasPacket::CommandType command = static_cast<const DasPacket::CommandType>(cmd & 0xFF);
     if (isDsp == 1)
-        packet = DasPacket::createOcc(DasPacket::HWID_SELF, m_hardwareId, command, channel, m_lastConfigPayloadLen, m_lastConfigPayload);
+        packet = DasPacket::createOcc(DasPacket::HWID_SELF, dest, command, 0, 0);
     else
-        packet = DasPacket::createLvds(DasPacket::HWID_SELF, m_hardwareId, command, channel, m_lastConfigPayloadLen, m_lastConfigPayload);
+        packet = DasPacket::createLvds(DasPacket::HWID_SELF, dest, command, 0, 0);
 
-    if (packet) {
-        BasePlugin::sendToDispatcher(packet);
-        delete packet;
+    memset(m_rawPacket, 0, sizeof(m_rawPacket));
+    if (packet)
+        memcpy(m_rawPacket, packet, std::min((size_t)packet->length(), sizeof(m_rawPacket)));
 
-        m_expectedResponse = command;
+    setIntegerParam(RawPkt0,  m_rawPacket[0]);
+    setIntegerParam(RawPkt1,  m_rawPacket[1]);
+    setIntegerParam(RawPkt2,  m_rawPacket[2]);
+    setIntegerParam(RawPkt3,  m_rawPacket[3]);
+    setIntegerParam(RawPkt4,  m_rawPacket[4]);
+    setIntegerParam(RawPkt5,  m_rawPacket[5]);
+    setIntegerParam(RawPkt6,  m_rawPacket[6]);
+    setIntegerParam(RawPkt7,  m_rawPacket[7]);
+    setIntegerParam(RawPkt8,  m_rawPacket[8]);
+    setIntegerParam(RawPkt9,  m_rawPacket[9]);
+    setIntegerParam(RawPkt10, m_rawPacket[10]);
+    setIntegerParam(RawPkt11, m_rawPacket[11]);
+    setIntegerParam(RawPkt12, m_rawPacket[12]);
+    setIntegerParam(RawPkt13, m_rawPacket[13]);
+    setIntegerParam(RawPkt14, m_rawPacket[14]);
+    setIntegerParam(RawPkt15, m_rawPacket[15]);
+    setIntegerParam(RawPkt16, m_rawPacket[16]);
+    setIntegerParam(RawPkt17, m_rawPacket[17]);
+    callParamCallbacks();
+}
 
-        int nSent = 0;
-        getIntegerParam(TxCount, &nSent);
-        setIntegerParam(TxCount, ++nSent);
+void DebugPlugin::sendPacket()
+{
+    DasPacket *packet = reinterpret_cast<DasPacket *>(m_rawPacket);
+    BasePlugin::sendToDispatcher(packet);
+    m_expectedResponse = packet->cmdinfo.command;
 
-        // Invalidate all params
-        setIntegerParam(RspCmd,     0);
-        setIntegerParam(RspCmdAck,  0);
-        setIntegerParam(RspHwType,  0);
-        setStringParam(RspSrc,      "");
-        setStringParam(RspRouter,   "");
-        setStringParam(RspDest,     "");
-        setIntegerParam(RspLen,     0);
-        setIntegerParam(RspDataLen, 0);
-        m_payloadLen = 0;
+    int nSent = 0;
+    getIntegerParam(TxCount, &nSent);
+    setIntegerParam(TxCount, ++nSent);
 
-        callParamCallbacks();
-    }
+    // Invalidate all params
+    setIntegerParam(RspCmd,     0);
+    setIntegerParam(RspCmdAck,  0);
+    setIntegerParam(RspHwType,  0);
+    setStringParam(RspSrc,      "");
+    setStringParam(RspRouter,   "");
+    setStringParam(RspDest,     "");
+    setIntegerParam(RspLen,     0);
+    setIntegerParam(RspDataLen, 0);
+    m_payloadLen = 0;
+
+    callParamCallbacks();
 }
 
 void DebugPlugin::processData(const DasPacketList * const packetList)
@@ -155,7 +245,7 @@ void DebugPlugin::processData(const DasPacketList * const packetList)
         const DasPacket *packet = *it;
 
         // Silently skip packets we're not interested in
-        if (!packet->isResponse() || packet->getSourceAddress() != m_hardwareId)
+        if (!packet->isResponse() || packet->getResponseType() != m_expectedResponse)
             continue;
 
         if (response(packet))
@@ -185,17 +275,6 @@ bool DebugPlugin::response(const DasPacket *packet)
     // Cache the payload to read it through readOctet()
     m_payloadLen = std::min(packet->getPayloadLength()/4, static_cast<uint32_t>(sizeof(m_payload)));
     memcpy(m_payload, packet->getPayload(), m_payloadLen*4);
-
-    // Cache last READ_CONFIG response separately, allowing other commands to be executed
-    // before using this payload for writing configuration
-    // Don't enforce MAC address checking, allowing to transfer the same configuration to
-    // multiple boards of the same type.
-    if (responseCmd == DasPacket::CMD_READ_CONFIG) {
-        m_lastConfigPayloadLen = m_payloadLen * 4;
-        memcpy(m_lastConfigPayload, m_payload, m_lastConfigPayloadLen);
-    } else {
-        m_lastConfigPayloadLen = 0;
-    }
 
     return true;
 }
