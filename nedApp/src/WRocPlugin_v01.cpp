94fafcbbb3d532821a4c861e84a465849ea79257
Miljko Bobrek <vxo@bl100-vxo3.ornl.gov>
2019-07-26 12:28:17 -0400

    Changed config registers

--- nedApp/src/WRocPlugin_v01.cpp
+++ nedApp/src/WRocPlugin_v01.cpp
@@ -26,33 +26,33 @@ void WRocPlugin::createParams_v01()
     createRegParam("VERSION", "FwDay",  true, 1,  8, 16, 0, CONV_HEX2DEC);
     createRegParam("VERSION", "FwMonth",true, 1,  8, 24, 0, CONV_HEX2DEC);
 
-    createConfigParam("NewFormat",  '1', 0,  1, 0, 0);  
-    createConfigParam("TclkSel",    '1', 0,  1, 1, 0);   
-    createConfigParam("ChannelEn0", '1', 1,  1, 0, 1);
-    createConfigParam("ChannelEn1", '1', 1,  1, 1, 1);
-    createConfigParam("ChannelEn2", '1', 1,  1, 2, 1);
-    createConfigParam("ChannelEn3", '1', 1,  1, 3, 1);
-    createConfigParam("ChannelEn4", '1', 1,  1, 4, 1);
-    createConfigParam("ChannelEn5", '1', 1,  1, 5, 1);
-    createConfigParam("ChannelEn6", '1', 1,  1, 6, 1);
-    createConfigParam("ChannelEn7", '1', 1,  1, 7, 1);
-    createConfigParam("LoopbackEn0",'1', 1,  1, 8, 0);
-    createConfigParam("LoopbackEn1",'1', 1,  1, 9, 0);
-    createConfigParam("LoopbackEn2",'1', 1,  1,10, 0);
-    createConfigParam("LoopbackEn3",'1', 1,  1,11, 0);
-    createConfigParam("LoopbackEn4",'1', 1,  1,12, 0);
-    createConfigParam("LoopbackEn5",'1', 1,  1,13, 0);
-    createConfigParam("LoopbackEn6",'1', 1,  1,14, 0);
-    createConfigParam("LoopbackEn7",'1', 1,  1,15, 0);
-    createConfigParam("TestEn0",    '1', 1,  1,16, 0);
-    createConfigParam("TestEn1",    '1', 1,  1,17, 0);
-    createConfigParam("TestEn2",    '1', 1,  1,18, 0);
-    createConfigParam("TestEn3",    '1', 1,  1,19, 0);
-    createConfigParam("TestEn4",    '1', 1,  1,20, 0);
-    createConfigParam("TestEn5",    '1', 1,  1,21, 0);
-    createConfigParam("TestEn6",    '1', 1,  1,22, 0);
-    createConfigParam("TestEn7",    '1', 1,  1,23, 0);
-    createConfigParam("TestRate",   '1', 1,  8,24, 100);
+    createConfigParam("Format",     '1', 0,  1, 0, 0);  // Format Select          (0=new,1=legacy)
+    createConfigParam("TclkSource", '1', 0,  1, 1, 1);  // Tclk Source Select     (0=internal,1=external)
+    createConfigParam("ChannelEn0", '1', 1,  1, 0, 1);  // Channel0 Enable        (0=disable,1=enable)
+    createConfigParam("ChannelEn1", '1', 1,  1, 1, 1);  // Channel1 Enable        (0=disable,1=enable)
+    createConfigParam("ChannelEn2", '1', 1,  1, 2, 1);  // Channel2 Enable        (0=disable,1=enable)
+    createConfigParam("ChannelEn3", '1', 1,  1, 3, 1);  // Channel3 Enable        (0=disable,1=enable)
+    createConfigParam("ChannelEn4", '1', 1,  1, 4, 1);  // Channel4 Enable        (0=disable,1=enable)
+    createConfigParam("ChannelEn5", '1', 1,  1, 5, 1);  // Channel5 Enable        (0=disable,1=enable)
+    createConfigParam("ChannelEn6", '1', 1,  1, 6, 1);  // Channel6 Enable        (0=disable,1=enable)
+    createConfigParam("ChannelEn7", '1', 1,  1, 7, 1);  // Channel7 Enable        (0=disable,1=enable)
+    createConfigParam("LoopbackEn0",'1', 1,  1, 8, 0);  // Loopback0 Enable       (0=disable,1=enable)
+    createConfigParam("LoopbackEn1",'1', 1,  1, 9, 0);  // Loopback1 Enable       (0=disable,1=enable)
+    createConfigParam("LoopbackEn2",'1', 1,  1,10, 0);  // Loopback2 Enable       (0=disable,1=enable)
+    createConfigParam("LoopbackEn3",'1', 1,  1,11, 0);  // Loopback3 Enable       (0=disable,1=enable)
+    createConfigParam("LoopbackEn4",'1', 1,  1,12, 0);  // Loopback4 Enable       (0=disable,1=enable)
+    createConfigParam("LoopbackEn5",'1', 1,  1,13, 0);  // Loopback5 Enable       (0=disable,1=enable)
+    createConfigParam("LoopbackEn6",'1', 1,  1,14, 0);  // Loopback6 Enable       (0=disable,1=enable)
+    createConfigParam("LoopbackEn7",'1', 1,  1,15, 0);  // Loopback7 Enable       (0=disable,1=enable)
+    createConfigParam("TestEn0",    '1', 1,  1,16, 0);  // Test0 Enable           (0=disable,1=enable)
+    createConfigParam("TestEn1",    '1', 1,  1,17, 0);  // Test1 Enable           (0=disable,1=enable)
+    createConfigParam("TestEn2",    '1', 1,  1,18, 0);  // Test2 Enable           (0=disable,1=enable)
+    createConfigParam("TestEn3",    '1', 1,  1,19, 0);  // Test3 Enable           (0=disable,1=enable)
+    createConfigParam("TestEn4",    '1', 1,  1,20, 0);  // Test4 Enable           (0=disable,1=enable)
+    createConfigParam("TestEn5",    '1', 1,  1,21, 0);  // Test5 Enable           (0=disable,1=enable)
+    createConfigParam("TestEn6",    '1', 1,  1,22, 0);  // Test6 Enable           (0=disable,1=enable)
+    createConfigParam("TestEn7",    '1', 1,  1,23, 0);  // Test7 Enable           (0=disable,1=enable)
+    createConfigParam("TestPeriod", '1', 1,  8,24, 100);// Test Period            (unit=ns)
 
     createStatusParam("SfpLoss0",      0,  1, 0);
     createStatusParam("SfpLoss1",      0,  1, 1);
